3631f5136f898c918808edc50a6abb8847bb7be9
Udit Narayan Pandey <uditpandit343@gmail.com>
2025-12-06 22:24:48 +0530

    AuthContext

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,14 @@ import './App.css'
 import {Route,BrowserRouter as  Router,Routes} from 'react-router-dom';
 import LandingPage from './Pages/landing'
 import Authentication from './Pages/authentication'
+import { AuthProvider } from './contexts/AuthContext';
 
 function App() {
 
   return (
     <>
      <Router>
+      <AuthProvider>
       <Routes>
         
             <Route path='/' element={<LandingPage/>}/>
@@ -16,7 +18,7 @@ function App() {
             <Route path='/auth' element={<Authentication/>}/>
 
       </Routes>
-      
+      </AuthProvider>
      </Router>
     </>
   )
